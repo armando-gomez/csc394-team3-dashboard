0b7730740c6aa85d0798912251579aeed56ee458
Armando Gomez <armando.0196@gmail.com>
2020-01-19 20:47:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -23,24 +23,24 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 8080);
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-	if (err) {
-		console.log(err);
-		process.exit(1);
-	}
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+// 	if (err) {
+// 		console.log(err);
+// 		process.exit(1);
+// 	}
 
-	// Save database object from the callback for reuse.
-	db = client.db();
-	console.log("Database connection ready");
+// 	// Save database object from the callback for reuse.
+// 	db = client.db();
+// 	console.log("Database connection ready");
 
-	// Initialize the app.
-	var server = app.listen(process.env.PORT || 8080, function () {
-		var port = server.address().port;
-		console.log("App now running on port", port);
-	});
-});
+// 	// Initialize the app.
+// 	var server = app.listen(process.env.PORT || 8080, function () {
+// 		var port = server.address().port;
+// 		console.log("App now running on port", port);
+// 	});
+// });
 
-function handleError(res, reason, message, code) {
-	console.log("ERROR: " + reason);
-	res.status(code || 500).json({ "error": message });
-}
+// function handleError(res, reason, message, code) {
+// 	console.log("ERROR: " + reason);
+// 	res.status(code || 500).json({ "error": message });
+// }
