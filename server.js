f564d5c8e682f9b65876e0ac1080d35e44e4054c
Armando Gomez <armando.0196@gmail.com>
2020-01-20 01:35:41 -0600

    server mongodb start

--- server.js
+++ server.js
@@ -11,32 +11,32 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/dist/csc394-team3-dashboard'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname + '/dist/csc394-team3-dashboard/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
 
 // // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-// var db;
+var db;
 
 // // Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-// 	if (err) {
-// 		console.log(err);
-// 		process.exit(1);
-// 	}
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
 
-// 	// Save database object from the callback for reuse.
-// 	db = client.db();
-// 	console.log("Database connection ready");
+	// 	// Save database object from the callback for reuse.
+	// 	db = client.db();
+	// 	console.log("Database connection ready");
 
-// 	// Initialize the app.
-// 	var server = app.listen(process.env.PORT || 8080, function () {
-// 		var port = server.address().port;
-// 		console.log("App now running on port", port);
-// 	});
-// });
+	// 	// Initialize the app.
+	// 	var server = app.listen(process.env.PORT || 8080, function () {
+	// 		var port = server.address().port;
+	// 		console.log("App now running on port", port);
+	// 	});
+});
 
 // // CONTACTS API ROUTES BELOW
 
