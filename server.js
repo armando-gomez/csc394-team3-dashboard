99343a83bc0b91ba918698150e13f346dcfe478e
Armando Gomez <armando.0196@gmail.com>
2020-01-19 20:40:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var db;
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/csc394-team3-dashboard'));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
 	// Replace the '/dist/<to_your_project_name>/index.html'
 	res.sendFile(path.join(__dirname + '/dist/csc394-team3-dashboard/index.html'));
 });
