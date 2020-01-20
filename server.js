70d3d6d7db50b3f43e2cb7baf07a62041217d785
Armando Gomez <armando.0196@gmail.com>
2020-01-19 18:47:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var app = express();
 app.use(bodyParser.json());
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/csc394-team3-dashboard'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/csc394-team3-dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
