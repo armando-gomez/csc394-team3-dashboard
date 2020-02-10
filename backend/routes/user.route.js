4b2bd287cf1eb8ecd7307a699eab29b749c4a876
Armando Gomez <armando.0196@gmail.com>
2020-02-09 22:50:14 -0600

    server.js rewrite

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const app = express();
-const userRoute = express.Router();
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
-// Employee model
 let User = require('../models/User');
 
-// Add Employee
-userRoute.route('/register').get(function (req, res, next) {
+router.post('/register', (req, res, next) => {
 	res.send('hello world');
-	// User.create(req.body, (error, data) => {
-	// 	if (error) {
-	// 		return next(error)
-	// 	} else {
-	// 		res.json(data)
-	// 	}
-	// });
-});
+})
 
 module.exports = userRoute;
--- server.js
+++ server.js
@@ -1,36 +1,44 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const path = require('path');
-let dbConfig = require("./backend/database/db");
+const passport = require('passport');
+const config = require("./backend/config/database");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-	useNewUrlParser: true
+mongoose.connect(config.db, {
+	useMongoClient: true
 }).then(() => {
 	console.log('Database sucessfully connected')
 },
 	error => {
 		console.log('Database could not connected: ' + error)
 	}
-)
-const userRoute = require('./backend/routes/user.route');
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(express.static(__dirname + '/dist/csc394-team3-dashboard'));
-app.use(cors());
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname + '/dist/csc394-team3-dashboard/index.html'));
-});
-app.use("/api", userRoute);
-app.listen(process.env.PORT || 8080);
+);
 
-app.use(function (err, req, res, next) {
-	console.error(err.message); // Log error message in our server's console
-	if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-	res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+const userRoute = require('./backend/routes/user.route');
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(__dirname + 'public'));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use("/api", userRoute);
+
+app.get('/', (req, res) => {
+	res.send("invalid enpoint");
 });
+
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname + 'public/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
