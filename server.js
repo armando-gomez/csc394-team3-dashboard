574056df44be1c4d9784d93a3a868f916b03da6f
Armando Gomez <armando.0196@gmail.com>
2020-02-09 19:09:42 -0600

    Revert "Update server.js"
    This reverts commit 7ae07b2385f5737108068f8191824d051277f23f.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
 		process.exit(1);
 	}
 
-	// Save database object from the callback for reuse
+	// Save database object from the callback for reuse.
 	db = client.db();
 	console.log("Database connection ready");
 
