100b235f8d0ff72eb50ac37ab981a943d4df5773
Armando Gomez <armando.0196@gmail.com>
2020-02-27 12:38:22 -0600

    Update jobs.component.ts

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from 'src/app/services/job.service';
 import { JobPost } from "../../interfaces/job-post";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
 	selector: 'app-jobs',
@@ -11,14 +14,21 @@ export class JobsComponent implements OnInit {
 	jobs: JobPost[];
 
 	constructor(
-		private jobService: JobService
+		private jobService: JobService,
+		private http: HttpClient,
+		private authService: AuthService
 	) { }
 
 	ngOnInit() {
 	}
 
 	loadJobPosts() {
-		this.jobService.getAllJobPosts().subscribe((data: JobPost) => console.log(data));
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Authorization': this.authService.getToken()
+			})
+		};
+		this.http.get<JobPost>('job/all', httpOptions).subscribe(data => console.log(data));
 	}
 
 }
