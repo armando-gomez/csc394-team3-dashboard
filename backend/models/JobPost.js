201e0f794c82a5ea53b7935ff8e4bbaa519df4c9
marcinfilipek1 <49353886+marcinfilipek1@users.noreply.github.com>
2020-03-08 12:50:58 -0500

    Update JobPost.js

--- backend/models/JobPost.js
+++ backend/models/JobPost.js
@@ -45,6 +45,32 @@ module.exports.getJobPostByLink = function (link, callback) {
 	JobPost.findOne(query, callback);
 }
 
+module.exports.getJobPostByTitle = function (title, callback) {
+	const query = { Title: title };
+	JobPost.find(query, callback);
+}
+
+module.exports.getJobPostByCompany = function (company, callback) {
+	const query = { Company: company };
+	JobPost.find(query, callback);
+}
+
+module.exports.getJobPostByLocation = function (location, callback) {
+	const query = { Location: location };
+	JobPost.find(query, callback);
+}
+
+module.exports.getJobPostByPay = function (pay, callback) {
+	const query = { Pay: pay };
+	JobPost.find(query, callback);
+}
+
+module.exports.getJobPostByPosted = function (posted, callback) {
+	const query = { Posted: posted };
+	JobPost.find(query, callback);
+}
+
+
 module.exports.getAllJobPosts = function (filter, callback) {
 	JobPost.find(filter, callback);
 }
